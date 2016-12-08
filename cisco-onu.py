a49f54a7df6b0609901de5ec4d9401395aa20368
T K <tk@example.com>
2016-12-08 16:37:25 +0100

    added mutual exclusion of options -i FILE | -s SERIALNUMBER

--- cisco-onu.py
+++ cisco-onu.py
@@ -25,25 +25,33 @@ conf = dominant
 def check_arg(args=None):
 
     parser = argparse.ArgumentParser(description='Cisco OLT manipulator')
-    parser.add_argument('-H', '--host', 
+    maingroup = parser.add_argument_group(title='required')
+    maingroup.add_argument('-H', '--host', 
                         help='host ip', 
                         default='192.168.35.50')
-    parser.add_argument('-u', '--user',
+    maingroup.add_argument('-u', '--user',
                         help='user name',
                         default='admin')
-    parser.add_argument('-p', '--passwd',
+    maingroup.add_argument('-p', '--passwd',
                         help='password of the OLT',
                         required='True')
-    parser.add_argument('-i','--file',
+    exgroup = parser.add_argument_group(title='one or the other')
+    
+    group = exgroup.add_mutually_exclusive_group(required=True)
+    group.add_argument('-i','--file',
                         type=argparse.FileType('rt'),
-                        help='file os ONU serial numbers',
-                        default='onu-sn.txt')
+                        help='file os ONU serial numbers')
+    group.add_argument('-s','--serialnumber',
+                        help='ONU serial number',
+                        default=None)
 
     results = parser.parse_args(args)
+
     return (results.host,
             results.user,
             results.passwd,
-            results.file)
+            results.file,
+            results.serialnumber)
 
 def send_command(remote_conn, cmd):
     cmd = cmd.rstrip()
@@ -76,8 +84,13 @@ def connect(host, user, passwd):
 
 def main():
 
-    host, user, passwd, filename = check_arg(sys.argv[1:])
+    host, user, passwd, filename, serialnumber = check_arg(sys.argv[1:])
 
+    print host
+    print user
+    print passwd
+    print filename
+    print serialnumber
 
     ssh = connect(host, user, passwd)
     # Use invoke_shell to establish an 'interactive session'
@@ -88,8 +101,36 @@ def main():
 
     
     onuid = conf['startOnuID']
-    for line in filename:
-        line = line.rstrip('\n')
+
+    if serialnumber == None:
+
+        for line in filename:
+            line = line.rstrip('\n')
+            create_onu = "remote-eq/discovery/create --serial-number=" + line + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --admin=enable --profileID=" + str(conf['profileID']) + " --register-type=serial-number"
+            add_mgmt = "remote-eq/onu/services/add --serviceID=" + str(conf['mgmtID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['mgmtUP']) + " --admin=enable --ip-mgmt=enable --name=ip-mgmt"
+            add_pppoe = "remote-eq/onu/services/add --serviceID=" + str(conf['pppoeID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['pppoeUP']) + " --admin=enable --name=internet-pppoe"
+            add_igmp = "remote-eq/onu/services/add  --serviceID=" + str(conf['igmpID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" +str(conf['igmpUP']) + " --admin=enable --name=iptv-igmp"
+            add_mcast = "remote-eq/onu/services/add --serviceID=" + str(conf['mcastID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --admin=enable --name=iptv-multicast"        
+            
+            
+            print "Sending cmd:\n" + create_onu 
+            output = send_command(remote_conn, create_onu)
+            print output 
+            print "\nSending cmd:\n" +  add_mgmt
+            output = send_command(remote_conn, add_mgmt)
+            print output 
+            print "\nSending cmd:\n" +  add_pppoe
+            output = send_command(remote_conn, add_pppoe)
+            print  output 
+            print "\nSending cmd:\n" +  add_igmp
+            output = send_command(remote_conn, add_igmp)
+            print  output 
+            print "\nSending cmd:\n" +  add_mcast
+            output = send_command(remote_conn, add_mcast)
+            print output 
+            onuid += 1
+    else:
+        line = serialnumber.rstrip('\n')
         create_onu = "remote-eq/discovery/create --serial-number=" + line + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --admin=enable --profileID=" + str(conf['profileID']) + " --register-type=serial-number"
         add_mgmt = "remote-eq/onu/services/add --serviceID=" + str(conf['mgmtID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['mgmtUP']) + " --admin=enable --ip-mgmt=enable --name=ip-mgmt"
         add_pppoe = "remote-eq/onu/services/add --serviceID=" + str(conf['pppoeID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['pppoeUP']) + " --admin=enable --name=internet-pppoe"
@@ -112,7 +153,7 @@ def main():
         print "\nSending cmd:\n" +  add_mcast
         output = send_command(remote_conn, add_mcast)
         print output 
-        onuid += 1
+
        
     remote_conn.close()
 
