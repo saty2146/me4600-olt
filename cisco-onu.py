b6eeb18e445573407921319013d4271a745c9222
T K <tk@example.com>
2016-12-09 10:19:39 +0100

    Added multicast package to igmp service

--- cisco-onu.py
+++ cisco-onu.py
@@ -9,38 +9,40 @@ import socket
 
 dominant = { 
         "pon" : "pon.7",
-        "profileID" : 4,
-        "pppoeID" : 7,
-        "mgmtID" : 1,
-        "igmpID" : 5,
-        "mcastID" : 6,
-        "mgmtUP": 1,
-        "igmpUP": 1,
-        "pppoeUP": 6,
-        "startOnuID" : 10
+        "profileID" : "4",
+        "pppoeID" : "7",
+        "mgmtID" : "1",
+        "igmpID" : "5",
+        "mcastID" : "6",
+        "mgmtUP": "1",
+        "igmpUP": "1",
+        "pppoeUP": "6",
+        "startOnuID" : "12"
     }
-
-conf = dominant
 
 def check_arg(args=None):
 
     parser = argparse.ArgumentParser(description='Cisco OLT manipulator')
     maingroup = parser.add_argument_group(title='required')
     maingroup.add_argument('-H', '--host', 
-                        help='host ip', 
+                        help='OLT IP address', 
                         default='192.168.35.50')
     maingroup.add_argument('-u', '--user',
-                        help='user name',
+                        help='OLT user name',
                         default='admin')
     maingroup.add_argument('-p', '--passwd',
-                        help='password of the OLT',
+                        help='OLT password',
+                        required='True')
+    maingroup.add_argument('-o', '--objectprofile',
+                        choices=['dominant','jegeho'],
+                        help='OLT object profile',
                         required='True')
     exgroup = parser.add_argument_group(title='one or the other')
     
     group = exgroup.add_mutually_exclusive_group(required=True)
     group.add_argument('-i','--file',
                         type=argparse.FileType('rt'),
-                        help='file os ONU serial numbers')
+                        help='file name of ONU serial numbers')
     group.add_argument('-s','--serialnumber',
                         help='ONU serial number',
                         default=None)
@@ -50,14 +52,19 @@ def check_arg(args=None):
     return (results.host,
             results.user,
             results.passwd,
+            results.objectprofile,
             results.file,
             results.serialnumber)
 
 def send_command(remote_conn, cmd):
+    
+    print "Sending cmd:\n" + cmd 
     cmd = cmd.rstrip()
     remote_conn.send(cmd + '\n')
     time.sleep(1)
-    return remote_conn.recv(1000)
+    output = remote_conn.recv(1000)
+    print output
+    return True
 
 def connect(host, user, passwd):
     
@@ -82,22 +89,49 @@ def connect(host, user, passwd):
         sys.exit("Connection timeout")
         
 
+def create_commands(line,conf,onuid):
+
+    sn = line
+    onuid = onuid
+    pon = conf['pon']
+    profileID = conf['profileID']
+    pppoeID = conf['pppoeID']
+    mgmtID = conf['mgmtID']
+    igmpID = conf['igmpID']
+    mcastID = conf['mcastID']
+    mgmtUP = conf['mgmtUP']
+    igmpUP = conf['igmpUP']
+    pppoeUP = conf['pppoeUP']
+
+
+    create_onu = "remote-eq/discovery/create --serial-number=" + sn + " --port=" + pon + " --onuID=" + onuid + " --admin=enable --profileID=" + profileID + " --register-type=serial-number"
+    add_mgmt = "remote-eq/onu/services/add --serviceID=" + mgmtID + " --port=" + pon + " --onuID=" + onuid + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + mgmtUP + " --admin=enable --ip-mgmt=enable --name=ip-mgmt --serviceID-onu=1"
+    add_pppoe = "remote-eq/onu/services/add --serviceID=" + pppoeID + " --port=" + pon + " --onuID=" + onuid + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + pppoeUP + " --admin=enable --name=internet-pppoe --serviceID-onu=2"
+    add_igmp = "remote-eq/onu/services/add  --serviceID=" + igmpID + " --port=" + pon + " --onuID=" + onuid + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + igmpUP + " --admin=enable --name=iptv-igmp --serviceID-onu=3"
+    add_mcast = "remote-eq/onu/services/add --serviceID=" + mcastID + " --port=" + pon + " --onuID=" + onuid + " --add-onu-port=veip.1 --admin=enable --name=iptv-multicast --serviceID-onu=4"       
+    add_mcast_pkg = "remote-eq/onu/services/mcast-package/create --onuID=" + onuid + " --port=" + pon + " --serviceID-onu=3 --pkg-id=1"
+    
+    return (create_onu,
+            add_mgmt,
+            add_pppoe,
+            add_igmp,
+            add_mcast,
+            add_mcast_pkg) 
+
 def main():
 
-    host, user, passwd, filename, serialnumber = check_arg(sys.argv[1:])
-
-    print host
-    print user
-    print passwd
-    print filename
-    print serialnumber
+    host, user, passwd, objectprofile, filename, serialnumber = check_arg(sys.argv[1:])
+    
+    if objectprofile == 'dominant':
+        conf = dominant
+    else:
+        print "Error"
 
     ssh = connect(host, user, passwd)
     # Use invoke_shell to establish an 'interactive session'
     remote_conn = ssh.invoke_shell()
     print "Interactive SSH session established to %s\n" % host
-    output = send_command(remote_conn,"\n")
-    print output
+    send_command(remote_conn,"\n")
 
     
     onuid = conf['startOnuID']
@@ -106,54 +140,26 @@ def main():
 
         for line in filename:
             line = line.rstrip('\n')
-            create_onu = "remote-eq/discovery/create --serial-number=" + line + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --admin=enable --profileID=" + str(conf['profileID']) + " --register-type=serial-number"
-            add_mgmt = "remote-eq/onu/services/add --serviceID=" + str(conf['mgmtID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['mgmtUP']) + " --admin=enable --ip-mgmt=enable --name=ip-mgmt"
-            add_pppoe = "remote-eq/onu/services/add --serviceID=" + str(conf['pppoeID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['pppoeUP']) + " --admin=enable --name=internet-pppoe"
-            add_igmp = "remote-eq/onu/services/add  --serviceID=" + str(conf['igmpID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" +str(conf['igmpUP']) + " --admin=enable --name=iptv-igmp"
-            add_mcast = "remote-eq/onu/services/add --serviceID=" + str(conf['mcastID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --admin=enable --name=iptv-multicast"        
+            create_onu, add_mgmt, add_pppoe, add_igmp, add_mcast, add_mcast_pkg = create_commands(line, conf, onuid)
             
-            
-            print "Sending cmd:\n" + create_onu 
-            output = send_command(remote_conn, create_onu)
-            print output 
-            print "\nSending cmd:\n" +  add_mgmt
-            output = send_command(remote_conn, add_mgmt)
-            print output 
-            print "\nSending cmd:\n" +  add_pppoe
-            output = send_command(remote_conn, add_pppoe)
-            print  output 
-            print "\nSending cmd:\n" +  add_igmp
-            output = send_command(remote_conn, add_igmp)
-            print  output 
-            print "\nSending cmd:\n" +  add_mcast
-            output = send_command(remote_conn, add_mcast)
-            print output 
+            send_command(remote_conn, create_onu)
+            send_command(remote_conn, add_mgmt)
+            send_command(remote_conn, add_pppoe)
+            send_command(remote_conn, add_igmp)
+            send_command(remote_conn, add_mcast)
+            send_command(remote_conn, add_mcast_pkg)
             onuid += 1
     else:
         line = serialnumber.rstrip('\n')
-        create_onu = "remote-eq/discovery/create --serial-number=" + line + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --admin=enable --profileID=" + str(conf['profileID']) + " --register-type=serial-number"
-        add_mgmt = "remote-eq/onu/services/add --serviceID=" + str(conf['mgmtID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['mgmtUP']) + " --admin=enable --ip-mgmt=enable --name=ip-mgmt"
-        add_pppoe = "remote-eq/onu/services/add --serviceID=" + str(conf['pppoeID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" + str(conf['pppoeUP']) + " --admin=enable --name=internet-pppoe"
-        add_igmp = "remote-eq/onu/services/add  --serviceID=" + str(conf['igmpID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --encryption=disable --upstream-dba-profile-id=" +str(conf['igmpUP']) + " --admin=enable --name=iptv-igmp"
-        add_mcast = "remote-eq/onu/services/add --serviceID=" + str(conf['mcastID']) + " --port=" + str(conf['pon']) + " --onuID=" + str(onuid) + " --add-onu-port=veip.1 --admin=enable --name=iptv-multicast"        
+        create_onu, add_mgmt, add_pppoe, add_igmp, add_mcast, add_mcast_pkg = create_commands(line, conf, onuid)
         
         
-        print "Sending cmd:\n" + create_onu 
-        output = send_command(remote_conn, create_onu)
-        print output 
-        print "\nSending cmd:\n" +  add_mgmt
-        output = send_command(remote_conn, add_mgmt)
-        print output 
-        print "\nSending cmd:\n" +  add_pppoe
-        output = send_command(remote_conn, add_pppoe)
-        print  output 
-        print "\nSending cmd:\n" +  add_igmp
-        output = send_command(remote_conn, add_igmp)
-        print  output 
-        print "\nSending cmd:\n" +  add_mcast
-        output = send_command(remote_conn, add_mcast)
-        print output 
-
+        send_command(remote_conn, create_onu)
+        send_command(remote_conn, add_mgmt)
+        send_command(remote_conn, add_pppoe)
+        send_command(remote_conn, add_igmp)
+        send_command(remote_conn, add_mcast)
+        send_command(remote_conn, add_mcast_pkg)
        
     remote_conn.close()
 
