caf213e6ea70b804d064caea26c2d1ab9fdd2c13
saty2146 <tibor.klimo@gmail.com>
2017-06-15 13:25:38 +0200

    add_onu.py in class

--- add_onu.py
+++ add_onu.py
@@ -10,96 +10,31 @@ import socket
 import re
 from myobject import *
 
-def check_arg(args=None):
-
-    parser = argparse.ArgumentParser(description='Cisco OLT manipulator')
-    maingroup = parser.add_argument_group(title='required')
-    maingroup.add_argument('-u', '--user',
-                        help='OLT username',
-                        default='admin')
-    maingroup.add_argument('-p', '--passwd',
-                        help='OLT password',
-                        required='True')
-    maingroup.add_argument('-o', '--objectprofile', 
-                        choices=['dominant','jegeho_dole','jegeho_hore','slnecnice_3etapa_B1_B2','slnecnice_3etapa_B3_B4','primyte_1etapa_1vl','shc3_olt_pon7','shc3_olt_pon8'],
-                        help='OLT object profile',
-                        required='True')
-    exgroup = parser.add_argument_group(title='one or the other')
-    
-    group = exgroup.add_mutually_exclusive_group(required=True)
-    group.add_argument('-i','--file',
-                        type=argparse.FileType('rt'),
-                        help='file name of ONU serial numbers')
-    group.add_argument('-s','--serialnumber',
-                        help='ONU serial number',
-                        default=None)
-
-    results = parser.parse_args(args)
-
-    return (results.user,
-            results.passwd,
-            results.objectprofile,
-            results.file,
-            results.serialnumber)
-
-
 def main():
 
-    user, passwd, objectprofile, filename, serialnumber = check_arg(sys.argv[1:])
-    
-    if objectprofile == 'dominant':
-        conf = dominant
-    elif objectprofile == 'jegeho_dole':
-        conf = jegeho_dole
-    elif objectprofile == 'jegeho_hore':
-        conf = jegeho_hore
-    elif objectprofile == 'slnecnice_3etapa_B1_B2':
-        conf = slnecnice_3etapa_B1_B2
-    elif objectprofile == 'slnecnice_3etapa_B3_B4':
-        conf = slnecnice_3etapa_B3_B4
-    elif objectprofile == 'primyte_1etapa_1vl':
-        conf = primyte_1etapa_1vl
-    elif objectprofile == 'shc3_olt_pon7':
-        conf = shc3_olt_pon7
-    elif objectprofile == 'shc3_olt_pon8':
-        conf = shc3_olt_pon8
-    else:
-        print "No Object profile specified"
+    olt, profile, sn = mymod.olt_profile(mymod.check_arg)
 
-    host = conf['host']
-    ssh = mymod.connect(host, user, passwd)
-    # Use invoke_shell to establish an 'interactive session'
-    remote_conn = ssh.invoke_shell()
-    mymod.send_command(remote_conn)
-    
-    onuid = mymod.find_next_onu(remote_conn, conf)
+    if sn is not None:
+        myolt = mymod.Olt(olt, profile)
+        ssh = myolt.connect()
+        remote_conn = ssh.invoke_shell()
+        myolt.send_command(remote_conn)
 
-    if serialnumber == None:
-
-        for line in filename:
-            line = line.rstrip('\n')
-            create_onu, add_mgmt, add_pppoe, add_igmp, add_mcast, add_mcast_pkg = mymod.create_commands(line, conf, onuid)
-            
-            mymod.send_command(remote_conn, create_onu)
-            mymod.send_command(remote_conn, add_mgmt)
-            mymod.send_command(remote_conn, add_pppoe)
-            mymod.send_command(remote_conn, add_igmp)
-            mymod.send_command(remote_conn, add_mcast)
-            mymod.send_command(remote_conn, add_mcast_pkg)
-            onuid += 1
-    else:
-        line = serialnumber.rstrip('\n')
-        create_onu, add_mgmt, add_pppoe, add_igmp, add_mcast, add_mcast_pkg = mymod.create_commands(line, conf, onuid)
+        onu_id = myolt.find_next_onu(remote_conn)
+        
+        create_onu, add_mgmt, add_pppoe, add_igmp, add_mcast, add_mcast_pkg = myolt.create_commands(sn, onu_id)
         
         
-        mymod.send_command(remote_conn, create_onu)
-        mymod.send_command(remote_conn, add_mgmt)
-        mymod.send_command(remote_conn, add_pppoe)
-        mymod.send_command(remote_conn, add_igmp)
-        mymod.send_command(remote_conn, add_mcast)
-        mymod.send_command(remote_conn, add_mcast_pkg)
+        myolt.send_command(remote_conn, create_onu)
+        myolt.send_command(remote_conn, add_mgmt)
+        myolt.send_command(remote_conn, add_pppoe)
+        myolt.send_command(remote_conn, add_igmp)
+        myolt.send_command(remote_conn, add_mcast)
+        myolt.send_command(remote_conn, add_mcast_pkg)
       
-    remote_conn.close()
+        remote_conn.close()
+    else:
+        print ("!!! No ONU SN specified !!!")
 
 if __name__ == "__main__":
     main()
--- find_next_onu.py
+++ find_next_onu.py
@@ -1,43 +1,18 @@
 #!/usr/bin/python
 
 import mymod
-import paramiko
-import StringIO
-import sys, getopt
-import time
-import argparse
-import socket
-import re
-from myobject import *
-
-
-user = 'admin'
-passwd = 'LedaMetis2012'
-hostname = '192.168.35.51'
-
-#define custom conf dictionary
-
-#define well known dictionary from myobject or use custom conf
-conf = slnecnice_3etapa_B1_B2
-
-#custom conf{}
-
-#conf = { 'slot': '1',
-#        'pon': 'pon.6'
-#        }
-#
 
 def main():
     
-    ssh = mymod.connect(hostname, user, passwd)
+    olt, profile, sn = mymod.olt_profile(mymod.check_arg)
+    myolt = mymod.Olt(olt, profile)
+    ssh = myolt.connect()
     remote_conn = ssh.invoke_shell()
-    olt = mymod.Olt()
-    olt.send_command(remote_conn)
-    output = olt.find_next_onu(remote_conn, conf)
-    print output
+    myolt.send_command(remote_conn)
+    output = myolt.find_next_onu(remote_conn)
+    print "Next ONU ID: " + str(output)
 
     remote_conn.close()
-
 
 if __name__ == "__main__":
     main()
